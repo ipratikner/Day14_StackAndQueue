455b6ba3cadbd2b53089fa420bac6124fe5319bf
ipratikner <pratiknerlikar@gmail.com>
2021-12-07 11:42:45 +0530

    Added: Created queue

---
+++ src/main/java/com/bridgelabz/MyQueue.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+    public void enqueue(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue() {
+        myLinkedList.printmyNodes();
+    }
+}
---
+++ src/test/java/com/bridgelabz/MyQueueTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class MyQueueTest {
+    @Test
+    public void given3NumberWhenAddedtoQueueShouldHaveLastAddedNode(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+
+    }
+}
