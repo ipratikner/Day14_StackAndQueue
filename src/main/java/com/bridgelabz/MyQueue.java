6fa0cb5b90aa172afc63c2c11290a8030198741d
ipratikner <pratiknerlikar@gmail.com>
2021-12-07 11:47:03 +0530

    Added: Created dequeue

--- src/main/java/com/bridgelabz/MyQueue.java
+++ src/main/java/com/bridgelabz/MyQueue.java
@@ -13,4 +13,8 @@ public class MyQueue {
     public void printQueue() {
         myLinkedList.printmyNodes();
     }
+    public INode dequeue(){
+        INode tempNode = myLinkedList.pop();
+        return tempNode;
+    }
 }
--- src/test/java/com/bridgelabz/MyQueueTest.java
+++ src/test/java/com/bridgelabz/MyQueueTest.java
@@ -15,4 +15,18 @@ public class MyQueueTest {
         myQueue.printQueue();
 
     }
+    @Test
+    public void given3NumberWhenPoppedOuttoQueueShouldHaveFirstNode() {
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+        INode tempNode = myQueue.dequeue();
+        myQueue.printQueue();
+        System.out.println("Deleted Node: " + tempNode.getKey());
+    }
 }
