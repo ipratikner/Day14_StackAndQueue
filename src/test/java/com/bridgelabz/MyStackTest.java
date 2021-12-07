49f7ac819f367f6c3199f235642f124ebfb2b3a3
ipratikner <pratiknerlikar@gmail.com>
2021-12-07 11:32:17 +0530

    Added: Created Stack and pushed elements into the stack

---
+++ src/test/java/com/bridgelabz/MyStackTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackTest {
+    @Test
+    public void given3NumberWhenAddedtoStackShouldHaveLastAddedNode(){
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        myStack.printStack();
+        INode peak = myStack.peak();
+        Assertions.assertEquals(myThirdNode,peak);
+    }
+
+}
